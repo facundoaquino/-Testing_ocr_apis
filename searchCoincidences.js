f7a0a73a2ca7990dd0e5def9f43df0fb76b7afe4
Facundo Aquino <facun90@live.com>
2022-02-10 14:44:37 -0300

    trim extension function

---
+++ searchCoincidences.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+const fs = require('fs')
+
+const searchCoincidences = (dataExel = []) => {
+	console.log(dataExel)
+	const filesWhihoutDoc = fs.readdirSync(process.env.PATH_TO_DOWNLOAD)
+
+	const data = [...filesWhihoutDoc]
+	let counter = 0
+
+	for (let i = 0; i < dataExel.length; i++) {
+		const element = dataExel[i]
+		// console.log(element)
+		for (let j = 0; j < data.length; j++) {
+			const file = data[j]
+			if (file.includes(element.linea) && !file.includes('-D-')) {
+				console.log({ linea: element.dni, file })
+
+				const strfileSplited = file.split('x')
+
+				// console.log(strfileSplited)
+				let str = strfileSplited[0] + '-D-' + element.dni
+				if (strfileSplited.length > 1) {
+					strfileSplited.forEach((element, i) => {
+						if (i > 0) {
+							str = str + 'x'
+						}
+					})
+				}
+				counter++
+				data[j] = str
+			}
+		}
+	}
+	// console.log(indexesNot)
+	console.log(`Documento agregado en ${counter}  fotos`)
+	// console.log(data)
+	// indexesNot.forEach((p) => data.push(photos[p]))
+	// fs.writeFileSync('./test.json', JSON.stringify(data, null, 5))
+	return data
+}
+
+module.exports = {
+	searchCoincidences,
+}
